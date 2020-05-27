22a1844a93a23525605228a50aa3e8afd65162bc
unknown <e.skrynnyk@yandex.ru>
2020-05-27 22:03:14 +0300

    'kavuchki_init_py'

--- UMBot3.py
+++ UMBot3.py
@@ -39,8 +39,7 @@ class BotHandler:
 
 # In[ ]:
 
-token='1222181388:AAGJgJGKBcfGdpGGipYfCQC5f0ap5whmsT4'
-greet_bot = BotHandler(token)  
+greet_bot = BotHandler('1222181388:AAGJgJGKBcfGdpGGipYfCQC5f0ap5whmsT4')  
 greetings = ('hello', 'hi', 'greetings', 'sup')  
 now = datetime.datetime.now()
 
--- __init__.py
+++ __init__.py
@@ -1,11 +1,2 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
-
-
-git init
-git add .
-git commit -m ‘short message that describe changes to commit’
-git push -u https://github.com/eskrynnyk/repo_bot.git
 
