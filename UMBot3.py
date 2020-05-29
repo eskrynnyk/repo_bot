07cceb5a587d016afa0f83594e22e5ad59cce780
unknown <e.skrynnyk@yandex.ru>
2020-05-29 15:05:45 +0300

    'polling'

--- UMBot3.py
+++ UMBot3.py
@@ -19,4 +19,5 @@ def receive_update():
         send_message(chat_id, "pong")
     return {"ok": True}
 if __name__ == "__main__":
+    bot.polling(none_stop=True)
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
